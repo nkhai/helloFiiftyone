d1922ee3fccf67ad79a86358f5158fc6d5b9482b
vkn1hc <khai.vonguyen@vn.bosch.com>
2024-04-09 11:24:50 +0700

    update

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -5,9 +5,27 @@ import { useRecoilValue } from "recoil";
 export function Panel() {
   const dataset = useRecoilValue(fos.dataset);
   return (
-    <h1>
-      khai You are viewing the <strong>{dataset.name}</strong> dataset.
-    </h1>
+    <div>
+      <h1>
+        khai You are viewing the <strong>{dataset.name}</strong> dataset.
+      </h1>
+      <label for="cars">Choose a car:</label>
+
+      <select name="cars" id="cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+
+      <form action="/action_page.php">
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname"/><br/><br/>
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname"/><br/><br/>
+        <input type="submit" value="Submit"/>
+      </form>
+    </div>
   );
 }
 
