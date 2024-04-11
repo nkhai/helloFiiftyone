f2cb4b65215ce861bfcc4c7c85d3c40ebad5cbf8
Vo Nguyen Khai <khai.vonguyen@vn.bosch.com>
2024-04-11 14:06:33 +0700

    update

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -1,30 +1,28 @@
+// import React from 'react';
 import { registerComponent, PluginComponentType } from "@fiftyone/plugins";
 import * as fos from "@fiftyone/state";
 import { useRecoilValue } from "recoil";
+// import { Box } from '@mui/material/Box';
+// import { TextField } from '@mui/material/TextField';
+import { Box, Typography, TextField } from "@mui/material";
 
 export function Panel() {
   const dataset = useRecoilValue(fos.dataset);
   return (
     <div>
-    <h1>
-      khai You are viewing the <strong>{dataset.name}</strong> dataset.
-    </h1>
-    <label for="cars">Choose a car:</label>
-
-    <select name="cars" id="cars">
-      <option value="volvo">Volvo</option>
-      <option value="saab">Saab</option>
-      <option value="mercedes">Mercedes</option>
-      <option value="audi">Audi</option>
-    </select>
-
-    <form action="/action_page.php">
-      <label for="fname">First name:</label>
-      <input type="text" id="fname" name="fname"/><br/><br/>
-      <label for="lname">Last name:</label>
-      <input type="text" id="lname" name="lname"/><br/><br/>
-      <input type="submit" value="Submit"/>
-    </form>
+      <div>Enter Attribute Name: *</div>
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
   </div>
   );
 }
